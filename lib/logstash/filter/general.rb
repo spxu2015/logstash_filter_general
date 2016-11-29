e2632075f247335aea66a16690de6ec7331cd2f7
xushengping <xushengping@wanda.cn>
2016-11-29 15:09:06 +0800

    general.rb

---
+++ lib/logstash/filter/general.rb
@@ -0,0 +1,48 @@
+# encoding: utf-8
+require "logstash/filters/base"
+require "logstash/namespace"
+
+# This example filter will replace the contents of the default
+# message field with whatever you specify in the configuration.
+#
+# It is only intended to be used as an example.
+class LogStash::Filters::General < LogStash::Filters::Base
+
+  # Setting the config_name here is required. This is how you
+  # configure this filter from your Logstash config.
+  #
+  # filter {
+  #   general {
+  #     message => "My message..."
+  #   }
+  # }
+  #
+  config_name "general"
+
+  # Replace the message with this value.
+  config :message, :validate => :string, :default => "Hello World!"
+
+
+  public
+  def register
+    # Add instance variables
+  end # def register
+
+  public
+  def filter(event)
+
+    if @message
+      # Replace the event message with our message as configured in the
+      # config file.
+
+      # using the event.set API
+      event.set("message", @message)
+      # correct debugging log statement for reference
+      # using the event.get API
+      @logger.debug? && @logger.debug("Message is now: #{event.get("message")}")
+    end
+
+    # filter_matched should go in the last line of our successful code
+    filter_matched(event)
+  end # def filter
+end # class LogStash::Filters::Example
